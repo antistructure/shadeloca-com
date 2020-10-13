d66c13d6897e366fd00ff5573c618234febb80c5
antistructure <antistructure.tech@gmail.com>
2020-10-13 11:32:32 -0600

    added api

---
+++ .history/src/routes/_api_20201013000205.ts
@@ -0,0 +1,34 @@
+import fetch from 'node-fetch'
+
+const createProduct = (data: any) => 
+  fetch('/.netlify/functions/createProduct', {
+    body: JSON.stringify(data),
+    method: 'POST'
+  })
+  .then(response => {
+    return response.json()
+  })
+
+const getProducts = () =>
+  fetch('/.netlify/functions/getAllProducts')
+    .then((res) => res.json())
+
+const updateProduct = (id, data) =>
+  fetch(`/.netlify/functions/updateProductById/${id}`, {
+    body: JSON.stringify(data),
+    method: 'POST'
+  })
+  .then(res => res.json())
+
+const deleteProduct = (id) =>
+  fetch(`/.netlify/functions/deleteProductById/${id}`, {
+    method: 'POST',
+  })
+  .then(res => res.json())
+
+export default {
+  createProduct: createProduct,
+  getProducts: getProducts,
+  updateProduct: updateProduct,
+  deleteProduct: deleteProduct
+}
---
+++ .history/src/routes/products/index_20201013112843.svelte
@@ -0,0 +1,13 @@
+<script context="module">
+  export async function preload(page, session) {
+    const res = await this.fetch('/products.json')
+    const products = await res.json()
+
+    return { products }
+  }
+</script>
+
+<script>
+  export let products
+  console.log(products)
+</script>
